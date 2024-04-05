0215e5878c1fcf1a595d8c527882aa948a752de8
Eduardo Borges <113848266+EduardoBorges0@users.noreply.github.com>
2024-04-05 17:18:34 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Qual número você quer verificar?");
         int n = sc.nextInt();
         double calcOne = Math.sqrt(5 * Math.pow(n, 2) + 4);
         double calcTwo = Math.sqrt(5 * Math.pow(n, 2) - 4);
